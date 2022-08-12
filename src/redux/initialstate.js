6543e1d640af7989681c478c7f77803aab3b0ccd
GubalaJakub1999 <100382090+GubalaJakub1999@users.noreply.github.com>
2022-08-12 18:53:52 +0200

    setpu app

---
+++ src/redux/initialstate.js
@@ -0,0 +1,6 @@
+const initialState = {
+  rases: ['Elf', 'High Elf', 'Dark Elf', 'Org', 'Human', 'Lizard'],
+  worlds: ['Hell', 'Heaven', 'Earth'],
+};
+
+export default initialState;
---
+++ src/redux/store.js
@@ -0,0 +1,21 @@
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
+import initialState from './initialstate';
+import thunk from 'redux-thunk';
+
+const subreducers = {
+
+}
+
+
+const reducer = combineReducers(subreducers);
+
+const store = createStore(
+  reducer,
+  initialState,
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
+  )
+);
+
+export default store;
